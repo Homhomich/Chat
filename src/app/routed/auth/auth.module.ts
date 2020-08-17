ac7508f35f50e3ea8ace06a4eb9281bc903f3689
Vlad Kolesnikov <vkoles2611@gmail.com>
2020-08-17 14:28:42 +0300

    Content + styles.

--- src/app/routed/auth/auth.module.ts
+++ src/app/routed/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPage } from './pages/login/login.page';
 
 @NgModule({
-  declarations: [ LoginPage],
+  declarations: [LoginPage],
   imports: [CommonModule, AuthRoutingModule]
 })
 export class AuthModule {}
--- src/app/routed/auth/pages/login/login.page.ts
+++ src/app/routed/auth/pages/login/login.page.ts
@@ -1,22 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   addClass(id: string, other: string): void {
     const button = document.getElementById(id);
     if (button && !button.className.includes(' clicked')) {
       button.className += ' clicked';
     }
-
 
     const otherButton = document.getElementById(other);
     if (otherButton) {
