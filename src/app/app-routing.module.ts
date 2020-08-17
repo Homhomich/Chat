7df680577b0f92708a6d96a8db43474fb258be7f
Vlad Kolesnikov <vkoles2611@gmail.com>
2020-08-17 10:25:38 +0300

    Icons.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPage } from './routed/chat/pages/main/main.page';
-import { FriendsPage } from './routed/chat/pages/friends/friends.page';
 import { ProfilePage } from './routed/chat/pages/profile/profile.page';
-import { LoginPage } from './routed/auth/pages/login/login.page';
 
 const routes: Routes = [
   {
     path: '',
     component: MainPage
-  },
-  {
-    path: 'friends',
-    component: FriendsPage
   },
   {
     path: 'profile',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPage } from './routed/chat/pages/main/main.page';
-import { FriendsPage } from './routed/chat/pages/friends/friends.page';
 import { ProfilePage } from './routed/chat/pages/profile/profile.page';
 
 @NgModule({
-  declarations: [AppComponent, MainPage, FriendsPage, ProfilePage],
+  declarations: [AppComponent, MainPage, ProfilePage],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
