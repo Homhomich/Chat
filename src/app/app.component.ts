1eb316a14168ca344e3fbbbb438d390249f2c680
anna <anya_homhomich@mail.ru>
2020-08-17 11:24:46 +0300

    Made a grid for chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,5 @@ export class AppComponent {
   title = 'Chat';
   clicked = 'active';
 
-  addClass(id: string, other: string): void {
-    const button = document.getElementById(id);
-    if (!button.className.includes(' clicked')) {
-      button.className = button.className + ' clicked';
-    }
 
-
-    const otherButton = document.getElementById(other);
-    otherButton.className = otherButton.className.replace(' clicked', '');
-  }
 }
--- src/app/routed/auth/auth.module.ts
+++ src/app/routed/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { RegisterPage } from './pages/register/register.page';
 import { LoginPage } from './pages/login/login.page';
 
 @NgModule({
-  declarations: [RegisterPage, LoginPage],
+  declarations: [ LoginPage],
   imports: [CommonModule, AuthRoutingModule]
 })
 export class AuthModule {}
