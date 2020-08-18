b349dc770b67b372bc7865619f68fba49d91cef4
Vlad Kolesnikov <vkoles2611@gmail.com>
2020-08-18 22:01:41 +0300

    Responsible sidenav.

--- src/app/routed/chat/pages/profile/profile.page.ts
+++ src/app/routed/chat/pages/profile/profile.page.ts
@@ -5,7 +5,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss']
 })
 export class ProfilePage implements OnInit {
+  private hamburgerBtnChats: HTMLElement | null | undefined;
+  private chats: HTMLElement | null | undefined;
+  private hamburgerBtnFriends: HTMLElement | null | undefined;
+  private friends: HTMLElement | null | undefined;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.hamburgerBtnChats = document.getElementById('hamburgerBtnChats');
+    this.chats = document.getElementById('chats');
+    this.hamburgerBtnChats?.addEventListener('click', () => {
+      this.chats?.classList.toggle('open');
+    });
+    this.hamburgerBtnFriends = document.getElementById('hamburgerBtnFriends');
+    this.friends = document.getElementById('friends');
+    this.hamburgerBtnFriends?.addEventListener('click', () => {
+      this.friends?.classList.toggle('open');
+    });
+  }
 }
